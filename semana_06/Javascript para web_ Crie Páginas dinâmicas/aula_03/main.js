68f26e76225a10d75b3287d08d694fae08ba86ae
hectorcamp <hectorcamp@gmail.com>
2022-03-08 01:12:08 -0300

    Listas

---
+++ semana_06/Javascript para web_ Crie Páginas dinâmicas/aula_03/main.js
@@ -0,0 +1,9 @@
+//Som Pom------------------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
