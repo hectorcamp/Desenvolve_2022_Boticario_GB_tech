ad2e270e5634e149a3b1c12df730786adf664a15
hectorcamp <hectorcamp@gmail.com>
2022-03-08 02:55:17 -0300

    Iterando em listas

--- semana_06/Javascript para web_ Crie Páginas dinâmicas/aula_03/main.js
+++ semana_06/Javascript para web_ Crie Páginas dinâmicas/aula_03/main.js
@@ -1,9 +1,25 @@
-//Som Pom------------------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+//let idElementoAud
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
 
+//Enquanto--------------------------------------------
+while (contador < listaDeTeclas.length) {
+    
+    const instrumento = listaDeTeclas[contador].classList[1]
+
+    console.log;
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla');
+    }
+
+    contador = contador + 1;
+    
+    console.log(contador);
+
+}
---
+++ semana_06/Javascript para web_ Crie Páginas dinâmicas/aula_05/main.js
@@ -0,0 +1,42 @@
+//let idElementoAud
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//let contador = 0;
+
+//Enquanto--------------------------------------------
+/* while (contador < listaDeTeclas.length) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string-----------------------
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+    
+    //console.log(contador);
+
+} */
+
+//com for (para)
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+}
+
+    
