970abdb6c27af78ce0c55f50bc433fecfa581c7d
hectorcamp <hectorcamp@gmail.com>
2022-03-10 00:13:57 -0300

    Estruturas condicionais

--- semana_06/Javascript para web_ Crie Páginas dinâmicas/aula_05/main.js
+++ semana_06/Javascript para web_ Crie Páginas dinâmicas/aula_05/main.js
@@ -1,33 +1,17 @@
-//let idElementoAud
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+   
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();       
+    }           
+    else {
+        alert('Elemento não encontrado');
+        console.log('Elemento não encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//let contador = 0;
-
-//Enquanto--------------------------------------------
-/* while (contador < listaDeTeclas.length) {
-    
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    //template string-----------------------
-    const idAudio = `#som_${instrumento}`;
-
-    //console.log(idAudio);
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-
-    contador = contador + 1;
-    
-    //console.log(contador);
-
-} */
-
-//com for (para)
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
@@ -40,12 +24,10 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onkeydown = function (evento) {
 
-        console.log(evento.code)
-
-        if (evento.code === 'Space') {
+        if (evento.code === 'Space' || evento.code === 'Enter')  {
              tecla.classList.add('ativa');    
         }
-        
+               
     }
     
     tecla.onkeyup = function () {
