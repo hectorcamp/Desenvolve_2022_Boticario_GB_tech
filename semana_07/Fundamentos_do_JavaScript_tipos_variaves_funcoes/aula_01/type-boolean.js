05cbefb68024050f41a9f13f6b9004aaf275e2bc
hectorcamp <hectorcamp@gmail.com>
2022-03-10 03:00:21 -0300

    Tipos Booleanos

---
+++ semana_07/Fundamentos_do_JavaScript_tipos_variaves_funcoes/aula_01/type-boolean.js
@@ -0,0 +1,10 @@
+//Tipos Boleanos
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2);
