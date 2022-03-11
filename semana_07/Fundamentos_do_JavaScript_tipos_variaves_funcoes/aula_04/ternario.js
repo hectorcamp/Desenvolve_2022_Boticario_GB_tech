6bfc75db349719dae581ccb900f4ea90bd8bc35c
hectorcamp <hectorcamp@gmail.com>
2022-03-11 20:01:04 -0300

    Operador ternário

---
+++ semana_07/Fundamentos_do_JavaScript_tipos_variaves_funcoes/aula_04/ternario.js
@@ -0,0 +1,14 @@
+//Funciona com um if
+
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+// if (idadeCliente >= idadeMinima) {
+//     console.log("cerveja")
+// } else {
+//     console.log("suco")
+// }
+             //condição                    //true     //false                         
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
+
+
