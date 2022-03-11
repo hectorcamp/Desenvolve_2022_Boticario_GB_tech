79af847eb9870462b2dd68264413e2d0cebd7e53
hectorcamp <hectorcamp@gmail.com>
2022-03-11 01:27:29 -0300

    conversões

---
+++ semana_07/Fundamentos_do_JavaScript_tipos_variaves_funcoes/aula_02/conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 456;
+const numeroString = '456a';
+
+
+//console.log(numero + numeroString);
+
+// conversão explicíta
+
+// Number()
+console.log(numero + Number (numeroString));
+
+// String()
+//console.log(String(numero) + numeroString);
