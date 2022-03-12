ddaec019057653873ba840297cae9b5ca311d732
hectorcamp <hectorcamp@gmail.com>
2022-03-12 01:28:50 -0300

    Funções

---
+++ semana_07/Fundamentos_do_JavaScript_tipos_variaves_funcoes/aula_05/funcoes.js
@@ -0,0 +1,27 @@
+// Funções
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+//DECLARAÇÃO DE FUNÇÃO  
+
+// 1) Declara a função
+//********************//2 -> valor de soma()
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+
+// 2) Executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+// imprimeTexto("outro texto");
+
+//três formas de escrever funções
+
+function soma(){
+    //return tem que ser a última linha da função
+    return 2 + 2;
+}
+
+// console.log(soma());
