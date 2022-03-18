a4b986affe3cd05647cb7cddc491d7d9d7e841c2
hectorcamp <hectorcamp@gmail.com>
2022-03-18 00:49:15 -0300

    Manipulação de objetos

--- semana_08/Fundamentos_do_JavaScript_objetos/aula_02_Manipulacao_de_objetos/lista-dependentes.js
+++ semana_08/Fundamentos_do_JavaScript_objetos/aula_02_Manipulacao_de_objetos/lista-dependentes.js
@@ -16,4 +16,9 @@ const cliente = {
       parentesco:"filha",
       dataNasc:"04/01/2014"
   })
-  console.log(cliente)
+
+  //console.log(cliente)
+
+  const filhaMaisNova =cliente.dependentes.filter(dependente => dependente.dataNasc==="04/01/2014")
+
+  console.log(filhaMaisNova[0].nome)
