ed6954fc1860a9dc6aa0e4aaf7bd7a5ccd22a118
hectorcamp <hectorcamp@gmail.com>
2022-03-23 16:27:58 -0300

    Persistindo dados

--- semana_09/Rest_com_NodeJS_API_com_Express_MySQL/aula_03/controllers/atendimentos.js
+++ semana_09/Rest_com_NodeJS_API_com_Express_MySQL/aula_03/controllers/atendimentos.js
@@ -1,10 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send ('Você está na rota de atendimentos e está realizando um GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send ('Você está, na rota de atendimentos e está realizando um post')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send ('Post atendimento')
     })
 }
-
-
