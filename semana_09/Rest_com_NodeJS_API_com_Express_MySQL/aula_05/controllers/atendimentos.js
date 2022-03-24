725e3597d45d977012feacd038254f5c922d784c
hectorcamp <hectorcamp@gmail.com>
2022-03-23 23:47:47 -0300

    HTTP-GET

---
+++ semana_09/Rest_com_NodeJS_API_com_Express_MySQL/aula_05/controllers/atendimentos.js
@@ -0,0 +1,21 @@
+const atendimentos = require('../models/atendimentos')
+const Atendimento = require('../models/atendimentos')
+
+module.exports = app => {
+    app.get('/atendimentos', (req,res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id =parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+    })
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento, res)
+        
+    })
+}
