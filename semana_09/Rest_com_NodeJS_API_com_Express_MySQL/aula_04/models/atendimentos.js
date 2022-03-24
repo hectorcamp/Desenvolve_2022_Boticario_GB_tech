4589b15ba414b8a7d4a34e5ac4751829ac77531b
hectorcamp <hectorcamp@gmail.com>
2022-03-23 23:19:47 -0300

    Refatorando o código

--- semana_09/Rest_com_NodeJS_API_com_Express_MySQL/aula_04/models/atendimentos.js
+++ semana_09/Rest_com_NodeJS_API_com_Express_MySQL/aula_04/models/atendimentos.js
@@ -2,20 +2,44 @@ const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
 
-        const sql = 'INSERT INTO Atendimentos SET ? '
-        
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro) {
-                console.log(erro)
-            } else {
-                console.log(resultados)
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >=5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {        
+            const atendimentoDatado = {...atendimento, dataCriacao,data}
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
     }
 }
 
