4aa66bb05c16d83e82a6a23cb4dcea7d5fd0acc3
hectorcamp <hectorcamp@gmail.com>
2022-04-05 00:52:01 -0300

    Aprofundando o método GET

--- semana_10/NodeJs_crie_uma_api_rest_padronizada_e_escalavel/api-petshop/api/rotas/fornecedores/Fornecedor.js
+++ semana_10/NodeJs_crie_uma_api_rest_padronizada_e_escalavel/api-petshop/api/rotas/fornecedores/Fornecedor.js
@@ -1,3 +1,4 @@
+const { pegarPorId } = require('./TabelaFornecedor')
 const TabelaFornecedor = require('./TabelaFornecedor')
 
 class Fornecedor {
@@ -33,8 +34,26 @@ class Fornecedor {
         this.dataAtualizacao = encontrado.dataAtualizacao
         this.versao = encontrado.versao
     }
-}
 
-    
+    async atualizar () {
+        await TabelaFornecedor.pegarPorId(this.id)
+        const campos = ['empresa', 'email', 'categoria']
+        const dadosParaAtualizar = {}
+
+        campos.forEach((campo) => {
+            const valor = this[campo]
+
+            if (typeof valor === 'string' && valor.length > 0) {
+                dadosParaAtualizar[campo] = valor
+            }
+        })
+
+        if (Object.keys(dadosParaAtualizar) .length === 0) {
+            throw new Error('Não foram fornecidos dados para atualizar!')
+        }
+
+        await TabelaFornecedor.atualizar(this.id, dadosParaAtualizar)
+    }
+}
 
 module.exports = Fornecedor
