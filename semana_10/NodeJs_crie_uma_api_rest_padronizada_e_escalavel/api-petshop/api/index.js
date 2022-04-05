4cf83d0ab59751a116359611881f6b2f5156c650
hectorcamp <hectorcamp@gmail.com>
2022-04-04 22:37:03 -0300

    Conhecendo GET

--- semana_10/NodeJs_crie_uma_api_rest_padronizada_e_escalavel/api-petshop/api/index.js
+++ semana_10/NodeJs_crie_uma_api_rest_padronizada_e_escalavel/api-petshop/api/index.js
@@ -5,4 +5,7 @@ const config = require('config')
 
 app.use(bodyParser.json())
 
-app.listen(3000, () => console.log('A API está funcionando!'))
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador)
+
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
