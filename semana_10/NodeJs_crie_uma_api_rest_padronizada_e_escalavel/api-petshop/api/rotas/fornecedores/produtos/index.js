c3304607f617e5f02cb47799fb4a6c00d62b4dec
hectorcamp <hectorcamp@gmail.com>
2022-04-16 01:54:11 -0300

    Declarando a rota POST

--- semana_10/NodeJs_crie_uma_api_rest_padronizada_e_escalavel/api-petshop/api/rotas/fornecedores/produtos/index.js
+++ semana_10/NodeJs_crie_uma_api_rest_padronizada_e_escalavel/api-petshop/api/rotas/fornecedores/produtos/index.js
@@ -1,5 +1,6 @@
 const roteador = require('express').Router({ mergeParams: true })
 const Tabela = require('./TabelaProduto')
+const Produto = require('./Produto')
 
 roteador.get('/', async (requisicao, resposta) => {
     const produtos = await Tabela.listar(requisicao.params.idFornecedor)
@@ -7,5 +8,15 @@ roteador.get('/', async (requisicao, resposta) => {
         JSON.stringify(produtos)
     )
 })
+
+roteador.post('/', async (requisicao, resposta) => {
+    const idFornecedor = requisicao.params.idFornecedor
+    const corpo = requisicao.body
+    const dados = Object.assign({}, corpo, { fornecedor: idFornecedor })
+    const produto = new Produto(dados)
+    await produto.criar()
+    resposta.status(201)
+    resposta.send(produto)
+})     
 
 module.exports = roteador
