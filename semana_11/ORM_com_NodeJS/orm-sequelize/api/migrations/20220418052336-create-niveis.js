a1d7e6bfc14cf2291e8047e48159451b68d2ef99
hectorcamp <hectorcamp@gmail.com>
2022-04-18 11:03:43 -0300

    Referenciando Tabelas

--- semana_11/ORM_com_NodeJS/orm-sequelize/api/migrations/20220418052336-create-niveis.js
+++ semana_11/ORM_com_NodeJS/orm-sequelize/api/migrations/20220418052336-create-niveis.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Níveis', {
+    await queryInterface.createTable('Niveis', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Níveis');
+    await queryInterface.dropTable('Niveis');
   }
 };
