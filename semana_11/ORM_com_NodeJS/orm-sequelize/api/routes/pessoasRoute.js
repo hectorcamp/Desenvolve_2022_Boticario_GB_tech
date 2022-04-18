634a96f2c6ae30a7f971dee2537d36e5c6b5ec72
hectorcamp <hectorcamp@gmail.com>
2022-04-18 00:55:25 -0300

    Métodos de CRUD

--- semana_11/ORM_com_NodeJS/orm-sequelize/api/routes/pessoasRoute.js
+++ semana_11/ORM_com_NodeJS/orm-sequelize/api/routes/pessoasRoute.js
@@ -5,5 +5,6 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 
 module.exports = router
