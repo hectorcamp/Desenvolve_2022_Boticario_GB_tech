cde24bbdee0d813e5911b11c2434593abac8922b
hectorcamp <hectorcamp@gmail.com>
2022-04-18 02:19:35 -0300

    Atualizar e deletar

--- semana_11/ORM_com_NodeJS/orm-sequelize/api/routes/pessoasRoute.js
+++ semana_11/ORM_com_NodeJS/orm-sequelize/api/routes/pessoasRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { pegaTodasAsPessoas } = require('../controllers/PessoaController')
+const { pegaTodasAsPessoas, atualizaPessoa } = require('../controllers/PessoaController')
 const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
@@ -7,5 +7,7 @@ const router = Router()
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
 
 module.exports = router
