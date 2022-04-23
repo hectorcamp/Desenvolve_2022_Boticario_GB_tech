4ea68a0d8efafca4602bbb0ee3a8e0c104145de7
hectorcamp <hectorcamp@gmail.com>
2022-04-23 01:41:29 -0300

    Usando transações

--- semana_12/orm-sequelize/api/controllers/PessoaController.js
+++ semana_12/orm-sequelize/api/controllers/PessoaController.js
@@ -180,13 +180,13 @@ class PessoaController {
     const lotacaoTurma = 2
     try {
       const turmasLotadas = await database.Matriculas
-      .findAndCountAll({
+        .findAndCountAll({
           where: {
-          status: 'confirmado'
+            status: 'confirmado'
           },
-        attributes: ['turma_id'],
-        group: ['turma_id'],
-        having: Sequelize.literal(`count(turma_id) >= ${lotacaoTurma}`)
+          attributes: ['turma_id'],
+          group: ['turma_id'],
+          having: Sequelize.literal(`count(turma_id) >= ${lotacaoTurma}`)
         })
       return res.status(200).json(turmasLotadas.count)
     } catch (error) {
@@ -199,9 +199,9 @@ class PessoaController {
     try {
       database.sequelize.transaction(async transacao => {
         await database.Pessoas
-        .update( { ativo: false }, { where: { id: Number(estudanteId) } }, { transaction: transacao })
-      await database.Matriculas
-        .update({ status: 'cancelado' }, { where: { estudante_id: Number(estudanteId) } }, { transaction: transacao })
+          .update( { ativo: false }, { where: { id: Number(estudanteId) } }, { transaction: transacao })
+        await database.Matriculas
+          .update({ status: 'cancelado' }, { where: { estudante_id: Number(estudanteId) } }, { transaction: transacao })
         return res.status(200).json({ message: `matriculas ref. estudante ${estudanteId} canceladas`})  
       })
     } catch (error) {
