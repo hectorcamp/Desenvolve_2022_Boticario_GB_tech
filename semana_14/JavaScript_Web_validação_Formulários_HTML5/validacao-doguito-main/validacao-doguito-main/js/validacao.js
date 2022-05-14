3b7e0b7724fdfb6b08e947510c1f7f83ffce7938
hectorcamp <hectorcamp@gmail.com>
2022-05-14 00:31:07 -0300

    Customizando mensagem de erro

--- semana_14/JavaScript_Web_validação_Formulários_HTML5/validacao-doguito-main/validacao-doguito-main/js/validacao.js
+++ semana_14/JavaScript_Web_validação_Formulários_HTML5/validacao-doguito-main/validacao-doguito-main/js/validacao.js
@@ -4,11 +4,58 @@ export function valida(input) {
     if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
+
+    if(input.validity.valid) {
+        input.parentElement.classlist.remove('input-container--invalido')
+        input.parentElement.querySelector('input-mensagem-erro').innerHTML = 'mostraMensagemDeErro'
+    } else {
+        input.parentElement.classlist.add('input-container--invalido')
+        input.parentElement.querySelector('input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+        
+    }
+}
+
+const tiposDeErro = [ 
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio',
+        typeMismatch: 'O email digitado não é válido'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, dever conter pleo menos uma letra maiúscula, um número e não deve conter símnbolos'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento nõ pode estar vazio',
+        customError: 'Você deve ser maior de 18 anos para se cadastrar'
+    }
+    
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input)
 }
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+
+    return mensagem
+}
+
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
