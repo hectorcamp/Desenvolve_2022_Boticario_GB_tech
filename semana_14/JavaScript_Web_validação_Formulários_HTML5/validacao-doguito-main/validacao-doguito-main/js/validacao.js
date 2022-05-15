5e7440d9a259036d7ba2f01993f058ae58b4fd93
hectorcamp <hectorcamp@gmail.com>
2022-05-15 02:14:56 -0300

    Validando CEP

--- semana_14/JavaScript_Web_validação_Formulários_HTML5/validacao-doguito-main/validacao-doguito-main/js/validacao.js
+++ semana_14/JavaScript_Web_validação_Formulários_HTML5/validacao-doguito-main/validacao-doguito-main/js/validacao.js
@@ -40,6 +40,10 @@ const mensagensDeErro = {
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
         customError: 'O CPF digitado não é válido.' 
+    },
+    cep: {
+        valueMissing: 'O campo do CEP não pode estar vazio.',
+        patternMismatch: 'O CEP digitado não é válido.'
     }
 }
 
