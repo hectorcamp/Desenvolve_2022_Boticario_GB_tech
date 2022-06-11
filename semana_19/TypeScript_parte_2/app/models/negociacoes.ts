d49f72a49ac60451987a0c34889901ae190cadb1
hectorcamp <hectorcamp@gmail.com>
2022-06-11 02:39:54 -0300

    Semana_20

---
+++ semana_19/TypeScript_parte_2/app/models/negociacoes.ts
@@ -0,0 +1,13 @@
+import { Negociacao } from "./negociacao";
+
+export class Negociacoes {
+    private negociacoes: Negociacao[] = [];
+
+    public adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    public lista(): readonly Negociacao [] {
+        return this.negociacoes;
+    }
+}
---
+++ semana_19/TypeScript_parte_2/dist/js/views/view.js
@@ -0,0 +1,23 @@
+export class View {
+    constructor(seletor, escapar) {
+        this.escapar = false;
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento;
+        }
+        else {
+            throw Error('Seletor não existe no DO<M');
+        }
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }
+    update(model) {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template
+                .replace(/<script>[\s\S]*?<script>/, '');
+        }
+        this.elemento.innerHTML = template;
+    }
+}
