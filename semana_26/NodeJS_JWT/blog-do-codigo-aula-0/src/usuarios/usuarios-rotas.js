c38eeda35b0d6bd907cfb0b65e4e63a76fa84309
hectorcamp <hectorcamp@gmail.com>
2022-08-04 23:50:41 -0300

    Implementando_autenticacao_com_JWT

--- semana_26/NodeJS_JWT/blog-do-codigo-aula-0/src/usuarios/usuarios-rotas.js
+++ semana_26/NodeJS_JWT/blog-do-codigo-aula-0/src/usuarios/usuarios-rotas.js
@@ -1,10 +1,15 @@
 const usuariosControlador = require('./usuarios-controlador');
+const middlewaresAutenticacao = require ('./middlewares-autenticacao');
 const passport = require('passport');
+
 
 module.exports = app => {
   app
     .route('/usuario/login')
-    .post(passport.authenticate('local', { session: false }), usuariosControlador.login);
+    .post(
+      middlewaresAutenticacao.local,
+      usuariosControlador.login
+      );
 
   app
     .route('/usuario')
