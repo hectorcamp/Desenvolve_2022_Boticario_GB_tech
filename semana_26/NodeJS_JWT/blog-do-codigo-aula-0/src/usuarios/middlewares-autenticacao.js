56e9e68d3b8ffee312f82686242210672707ff27
hectorcamp <hectorcamp@gmail.com>
2022-08-10 00:21:52 -0300

    Implementando_logout_com_tokens

--- semana_26/NodeJS_JWT/blog-do-codigo-aula-0/src/usuarios/middlewares-autenticacao.js
+++ semana_26/NodeJS_JWT/blog-do-codigo-aula-0/src/usuarios/middlewares-autenticacao.js
@@ -33,12 +33,11 @@ module.exports = {
           return res.status(401).json({ erro: erro.message });
         }
 
-        if (erro && erro.name === 'TokenExpriredError') {
+        if (erro && erro.name === 'TokenExpiredError') {
           return res
             .status(401)
             .json({ erro: erro.message, expiradoEm: erro.expiredAt });
         }
-
 
         if (erro) {
           return res.status(500).json({ erro: erro.message });
@@ -48,6 +47,7 @@ module.exports = {
           return res.status(401).json();
         }
 
+        req.token = info.token;
         req.user = usuario;
         return next();
       }
