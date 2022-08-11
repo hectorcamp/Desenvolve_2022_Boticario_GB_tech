65cd4eeafddb2841760d70e5490da80a4aa0daea
hectorcamp <hectorcamp@gmail.com>
2022-08-10 23:16:27 -0300

    Verificando_emails

--- semana_27/nodeJs_refresh_tokens_confimacao_cadastro/blog-do-codigo-2-aula0/src/usuarios/usuarios-controlador.js
+++ semana_27/nodeJs_refresh_tokens_confimacao_cadastro/blog-do-codigo-2-aula0/src/usuarios/usuarios-controlador.js
@@ -2,6 +2,12 @@ const Usuario = require('./usuarios-modelo');
 const { InvalidArgumentError } = require('../erros');
 
 const tokens = require('./tokens');
+const { EmailVerificacao } = require('./emails');
+
+function geraEndereco(rota, id) {
+  const baseURL = process.env.BASE_URL;
+  return `${baseURL}${rota}${id}`;
+}
 
 module.exports = {
   async adiciona(req, res) {
@@ -14,6 +20,10 @@ module.exports = {
       });
       await usuario.adicionaSenha(senha);
       await usuario.adiciona();
+
+      const endereco = geraEndereco('/usuario/verifica_email/', usuario.id);
+      const emailVerificacao = new EmailVerificacao(usuario, endereco);
+      emailVerificacao.enviaEmail().catch(console.log);
 
       res.status(201).json();
     } catch (erro) {
